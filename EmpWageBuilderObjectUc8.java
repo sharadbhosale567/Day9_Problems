d302aeeed01ea26e122116052d07058357ca9911
sharad <sharadbhosale567@gmail.com>
2022-03-25 14:45:41 +0530

    [Sharad]

--- EmpWageBuilderObjectUc8.java
+++ EmpWageBuilderObjectUc8.java
@@ -1,20 +1,27 @@
-package employwage;
+package employwagebuilder;
 
-public class EmpWageBuilderUC7 {
-
-    public static final int IS_FULL_TIME = 1;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int IS_PART_TIME = 2;
+class EmpWageBuilderObject1 {
+    public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR =20;
     public static final int WORK_DAY_PER_MONTH = 20;
     public static final int NUM_OF_WORKING_DAYS = 2;
     public static final int empWage = 0;
     public static final int totalEmpWage =0;
     public static final int WORKING_HORS_IN_MONTH = 100;
 
-    public static int calcEmplWageForCompany(String company, int empRate,
-                                             int numOfDays, int maxHrs) {
 
+    private final String company;
+    private final int empRatePerHour;
+    private final int numOfWorkingDays;
+    private final int maxHoursPerMonth;
 
+    public EmpWageBuilderObject1(String company, int empRatePerHour, int numOfWorkingDays,
+                                 int maxHoursPerMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
 
         int empHrs = 0;
         int empWage = 0;
@@ -35,19 +42,19 @@ public class EmpWageBuilderUC7 {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * empRate;
+            empWage = empHrs * empRatePerHour;
             totalEmpWage += empWage;
             System.out.println("Emp Wage : " +empWage);
         }
         System.out.println("Total Emp Wage for company : " +company + " is "  + totalEmpWage);
-        return totalEmpWage;
-
-//        int Total_emp_wage_per_Hrs = ( 100 * 20 );
-//        System.out.println("WORKING_HORS_IN_MONTH  : " +Total_emp_wage_per_Hrs);
-//
+        return;
     }
+
     public static void main(String[] args) {
-       calcEmplWageForCompany("CCM" ,20, 2,10);
-        calcEmplWageForCompany("Airtel" ,30, 2,8);
+
+        EmpWageBuilderObject1 object1 = new EmpWageBuilderObject1("CCM" ,20,
+                2,10);
+        EmpWageBuilderObject1 object2 = new EmpWageBuilderObject1("dMart" ,30,
+                5,8);
     }
 }
